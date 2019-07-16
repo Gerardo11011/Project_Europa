f2dfd4e3fa19d7be75dd9f2b0669df6e44913382
Gerardo Aldair Ponce Gomez <gerardo_apg_97@hotmail.com>
2019-07-16 15:29:08 +0200

    Cambios en los querys

--- routes/Variados.js
+++ routes/Variados.js
@@ -6,7 +6,7 @@ var db = mongoose.connection;
 var article = require ('../models/article')
 
 router.get('/', function (req, res, next) {
-  article.find({'tema': {$nin : ["Fisica", "Matematica", "Astronomia"]}}, function (err, article){
+  article.find({'tema': {$nin : ["Astronomy", "astronomy", "Astronomia", "astronomia","Math", "math", "Matematicas", "matematicas","physics", "Physics", "fisica", "Fisica"]}}, function (err, article){
 
     if(err){
        console.log(err);
